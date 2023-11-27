e0597e17e173fbaaea4f1be8b8a55d2da1595bb9
NvDzung <dung3032003md@gmail.com>
2023-11-27 15:19:39 +0700

    Thêm chức năng autoScroll cho ScrollPane.

--- src/main/java/manage/controller/MainController.java
+++ src/main/java/manage/controller/MainController.java
@@ -1,5 +1,9 @@
 package manage.controller;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import manage.data.HashMapStudent;
 import manage.data.SinhVien;
 import manage.database.ConnectDatabase;
@@ -27,9 +32,14 @@ public class MainController implements Initializable {
     @FXML
     private VBox nhanTinContent;
     @FXML
+    private VBox thongKeContent;
+    @FXML
     ScrollPane container;
     @FXML
     private Label UserName;
+
+    private static SimpleBooleanProperty autoScroll = new SimpleBooleanProperty(false);
+    private static double targetScrollPane = 0;
 
     HashMap<String, SinhVien> hashMapStudent = HashMapStudent.getHashSinhVien();
 
@@ -48,12 +58,26 @@ public class MainController implements Initializable {
         container.setContent(nhanTinContent);
     }
 
+    public static void setAutoScroll(boolean auto) {
+        autoScroll.set(auto);
+    }
+
+    public static void setTarget(double target) {
+        targetScrollPane = target;
+    }
+
+    @FXML
+    public void thongkeAction(ActionEvent event) {
+        container.setContent(thongKeContent);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
             trangChuContent = FXMLLoader.load(getClass().getResource("/Gui/HomePage_sub.fxml"));
             sinhVienContent = FXMLLoader.load(getClass().getResource("/Gui/sinhVien_sub.fxml"));
             nhanTinContent = FXMLLoader.load(getClass().getResource("/Gui/nhanTin_sub.fxml"));
+            thongKeContent = FXMLLoader.load(getClass().getResource("/Gui/ThongKe.fxml"));
             container.setContent(trangChuContent);
             String userName = LoginController.getUserName();
             UserName.setText(userName);
@@ -66,12 +90,12 @@ public class MainController implements Initializable {
             while (resultSet.next()) {
                 String maSv = resultSet.getString("id_sv");
                 String tenSv = resultSet.getString("ten_sv");
-                String ngaySinh = resultSet.getString("ngaysinh");
+                String ngaySinh = resultSet.getString("dob");
                 String gioiTinh = resultSet.getString("gioitinh");
                 String email = resultSet.getString("email");
                 String sdt = resultSet.getString("sodt");
-                String que = resultSet.getString("que");
-                String lop = resultSet.getString("id_lhc");
+                String que = resultSet.getString("quequan");
+                String lop = resultSet.getString("id_lophc");
 
                 SinhVien sv = new SinhVien(maSv, tenSv, ngaySinh, gioiTinh, email, sdt, que, lop);
                 hashMapStudent.put(maSv, sv);
@@ -82,6 +106,22 @@ public class MainController implements Initializable {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        autoScroll.addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                double scrollTime = 1000;                                                               // Định thời gian cho thanh cuộn chạy.
+                double target = targetScrollPane;                                                       // Định vị trí cuối cùng của thanh cuộn. (0.0 -> 1.0)
+
+                Timeline timeline = new Timeline(
+                        new KeyFrame(Duration.millis(scrollTime), new KeyValue(container.vvalueProperty(), target))
+                );
+
+                timeline.play();
+            } else {
+                container.vvalueProperty().unbind();                                                    // giải phóng bất kì ràng buộc nào được thiết lập cho thuộc tính vvalue của thanh cuộn.
+            }
+            setAutoScroll(false);
+        });
     }
 }
 
